b83bd62f323d5ecfb5be4e5e10c7c1262fdd84ea
axel.gr <axel.guillouard@lidix.io>
2022-12-07 16:10:00 +0100

    V2

--- inscription.php
+++ inscription.php
@@ -1,19 +1,36 @@
 <?php
+  // Récupération des données du formulaire
+  $prenom = $_POST['prenom'];
+  $nom = $_POST['nom'];
+  $sexe = $_POST['sexe'];
+  $naissance = $_POST['naissance'];
+  $email = $_POST['email'];
+  $diplome = $_POST['diplome'];
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From: Portfolio Website'; 
-    $to = 'kyle.a.binger@gmail.com'; 
-    $subject = 'Message From Personal Site';
+  // Connexion à la base de données
+  $host = "localhost";
+  $username = "mon_utilisateur";
+  $password = "mon_mot_de_passe";
+  $dbname = "ma_base_de_donnees";
 
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+  $conn = mysqli_connect($host, $username, $password, $dbname);
 
-    if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) { 
-            echo '<p>Your message has been sent!</p>';
-        } else { 
-            echo '<p>Something went wrong, go back and try again!</p>'; 
-        }
-    }
+  // Vérification de la connexion
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  // Préparation de la requête SQL d'insertion
+  $sql = "INSERT INTO formulaire_contact (prenom, nom, sexe, naissance, email, diplome) VALUES (?, ?, ?, ?, ?, ?)";
+  $stmt = mysqli_prepare($conn, $sql);
+
+  // Liaison des variables à la requête préparée
+  mysqli_stmt_bind_param($stmt, "sss", $prenom, $nom, $sexe, $naissance, $email, $diplome);
+
+  // Exécution de la requête
+  mysqli_stmt_execute($stmt);
+
+  // Fermeture de la requête et de la connexion
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
 ?>
