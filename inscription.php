af427f266b5d89822246ca22e4cebb39ea572c92
axel.gr <axel.guillouard@lidix.io>
2022-12-09 13:47:16 +0100

    up

--- inscription.php
+++ inscription.php
@@ -8,10 +8,10 @@
   $diplome = $_POST['diplome'];
 
   // Connexion à la base de données
-  $host = "localhost";
-  $username = "mon_utilisateur";
-  $password = "mon_mot_de_passe";
-  $dbname = "ma_base_de_donnees";
+  $host = "10.101.0.61";
+  $username = "root";
+  $password = "root";
+  $dbname = "inscription";
 
   $conn = mysqli_connect($host, $username, $password, $dbname);
 
@@ -21,11 +21,11 @@
   }
 
   // Préparation de la requête SQL d'insertion
-  $sql = "INSERT INTO formulaire_contact (prenom, nom, sexe, naissance, email, diplome) VALUES (?, ?, ?, ?, ?, ?)";
+  $sql = "INSERT INTO inscription (prenom, nom, sexe, naissance, email, diplome) VALUES (?,?,?,?,?,?)";
   $stmt = mysqli_prepare($conn, $sql);
 
   // Liaison des variables à la requête préparée
-  mysqli_stmt_bind_param($stmt, "sss", $prenom, $nom, $sexe, $naissance, $email, $diplome);
+  mysqli_stmt_bind_param($stmt, $prenom, $nom, $sexe, $naissance, $email, $diplome);
 
   // Exécution de la requête
   mysqli_stmt_execute($stmt);
@@ -33,4 +33,29 @@
   // Fermeture de la requête et de la connexion
   mysqli_stmt_close($stmt);
   mysqli_close($conn);
+ 
 ?>
+<!-- 
+<?php
+// Récupération des valeurs du formulaire HTML
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$sexe = $_POST['sexe'];
+$naissance = $_POST['naissance'];
+$email = $_POST['email'];
+$diplome = $_POST['diplome'];
+
+// Connexion à la base de données MySQL
+$host = 'localhost';
+$username = 'root';
+$password = 'root';
+$dbname = 'inscription';
+$mysqli = new mysqli($host, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($mysqli->connect_errno) {
+    echo "Échec de la connexion à la base de données MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    exit;
+}?>
+-->
+
